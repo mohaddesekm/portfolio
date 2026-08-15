5d2b8f7d7931ec7abd479acb094ec537c127fafc
Mohaddese Karimi <mhaddese@gmail.com>
2026-08-15 22:48:14 +0330

    feat: add portfolio color theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import './App.css';
 export default function App() {
     return (
         <>
-            <div className="min-h-screen bg-black flex items-center justify-center">
-                <h1 className="text-5xl font-bold text-white">My Portfolio</h1>
-            </div>
+            <main className="min-h-screen bg-primary-dark text-accent flex items-center justify-center">
+                <h1 className="text-5xl font-bold">My Portfolio</h1>
+            </main>
         </>
     );
 }
