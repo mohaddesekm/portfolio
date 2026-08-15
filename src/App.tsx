0c161cea0d3eb89421340735b8d28d89936353e3
Mohaddese Karimi <mhaddese@gmail.com>
2026-08-15 22:16:49 +0330

    chore: initialize React TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function App() {
+    return (
+        <>
+            <div className="min-h-screen bg-black flex items-center justify-center">
+                <h1 className="text-5xl font-bold text-white">My Portfolio</h1>
+            </div>
+        </>
+    );
+}
