6517b39a618a2e6575700aafd6826a1cb9654eaa
Mohaddese Karimi <mhaddese@gmail.com>
2026-08-17 01:35:29 +0330

    feat: add light and dark theme toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
-import { Menu, X } from 'lucide-react';
+import { useState, type Dispatch, type SetStateAction } from 'react';
+import { Menu, X, Sun, Moon } from 'lucide-react';
 
 type NavItem = {
     label: string;
     href: string;
+};
+
+type Theme = 'light' | 'dark';
+
+type NavbarProps = {
+    theme: Theme;
+    setTheme: Dispatch<SetStateAction<Theme>>;
 };
 
 const navItems: NavItem[] = [
@@ -14,17 +21,17 @@ const navItems: NavItem[] = [
     { label: 'Contact', href: '#contact' },
 ];
 
-export default function Navbar() {
+export default function Navbar({ theme, setTheme }: NavbarProps) {
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
     return (
         <>
-            <header className="fixed top-0 left-0 z-50 w-full border-b border-primary/30 bg-primary-dark/90 backdrop-blur-md">
+            <header className="fixed top-0 left-0 z-50 w-full border-b border-primary/20 bg-white-90 backdrop-blur-md dark:bg-primary-dark/90 dark:border-primary/30">
                 <nav className="mx-auto flex items-center justify-between px-4 sm:px-6 h-20 max-w-7xl lg:px-8">
                     {/* LOGO */}
                     <a
                         href="#home"
-                        className="text-2xl font-bold text-accent transition-colors hover:text-secondary"
+                        className="text-2xl font-bold text-primary dark:text-accent transition-colors"
                     >
                         KM.
                     </a>
@@ -36,7 +43,7 @@ export default function Navbar() {
                             <li key={item.href}>
                                 <a
                                     href={item.href}
-                                    className="text-sm font-medium text-accent transition-colors duration-300 hover:text-secondary"
+                                    className="text-sm font-medium text-primary dark:text-accent transition-colors duration-300 hover:text-secondary"
                                 >
                                     {item.label}
                                 </a>
@@ -47,9 +54,32 @@ export default function Navbar() {
                     <div className="flex items-center justify-center gap-5">
                         <button
                             type="button"
-                            className="rounded-lg border border-secondary/50 px-4 py-2 text-sm font-medium text-accent transition-colors hover:bg-primary"
+                            className="rounded-lg p-2 text-sm font-medium text-primary dark:text-accent transition-colors hover:bg-secondary/10 dark:hover:bg-primary outline-0 cursor-pointer"
+                            onClick={() =>
+                                setTheme((prev) =>
+                                    prev === 'dark' ? 'light' : 'dark',
+                                )
+                            }
                         >
-                            Theme
+                            <span className="relative block size-5">
+                                <Sun
+                                    size={20}
+                                    className={`absolute inset-0 transition-transform duration-300 ease-in-out ${
+                                        theme === 'dark'
+                                            ? 'rotate-0 opacity-100'
+                                            : 'rotate-180 opacity-0'
+                                    }`}
+                                />
+
+                                <Moon
+                                    size={20}
+                                    className={`absolute inset-0 transition-transform duration-300 ease-in-out ${
+                                        theme === 'dark'
+                                            ? 'rotate-180 opacity-0'
+                                            : 'rotate-0 opacity-100'
+                                    }`}
+                                />
+                            </span>
                         </button>
 
                         <button
@@ -63,14 +93,14 @@ export default function Navbar() {
                 </nav>
             </header>
             <div
-                className={`fixed w-full overflow-hidden transition-all duration-300 md:hidden bg-primary-dark shadow-lg shadow-black/40 rounded-b-2xl z-40 ${isMenuOpen ? 'max-h-96 opacity-100 top-20' : 'max-h-0 opacity-0 -top-full'}`}
+                className={`fixed w-full overflow-hidden transition-all duration-300 md:hidden bg-light-bg dark:bg-primary-dark shadow-lg shadow-primary-dark/15 dark:shadow-black/40 rounded-b-2xl z-40 ${isMenuOpen ? 'max-h-96 opacity-100 top-20' : 'max-h-0 opacity-0 -top-full'}`}
             >
                 <nav className="flex flex-col my-3">
                     <ul>
                         {navItems.map((item) => (
                             <li
                                 key={item.href}
-                                className="h-10 transition-all duration-200 hover:bg-primary/30 text-accent hover:text-secondary"
+                                className="h-10 transition-all duration-200 hover:bg-primary/10 dark:hover:bg-primary/30 text-primary dark:text-accent hover:text-secondary"
                             >
                                 <a
                                     href={item.href}
