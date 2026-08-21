a6f918338b10b535072b7a240f98cb0a677a8868
Mohaddese Karimi <mhaddese@gmail.com>
2026-08-21 23:25:27 +0330

    feat: complete hero right section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -50,9 +50,57 @@ export default function Hero({ language }: HeroProps) {
                 </div>
             </div>
 
-            <div className="flex w-full h-80 lg:h-full items-center justify-center">
-                <div className="w-full h-full max-w-xl rounded-2xl border border-primary/20 bg-primary/5 dark:border-accent/10 dark:bg-primary-dark/50"></div>
+            {/* <div className=" lg:h-full items-center justify-center"> */}
+            <div className="p-6 max-w-xl rounded-lg border border-primary/20 bg-primary/5 dark:border-accent/10 dark:bg-primary-dark/50">
+                <div className="flex items-center justify-between">
+                    <div className="flex space-x-2">
+                        <div className="w-3 h-3 rounded-full bg-red-500"></div>
+                        <div className="w-3 h-3 rounded-full bg-yellow-500"></div>
+                        <div className="w-3 h-3 rounded-full bg-green-500"></div>
+                    </div>
+                    <div className="text-sm text-gray-500">developer.js</div>
+                </div>
+                <div className="flex flex-col gap-2 text-sm">
+                    <div className="text-gray-500">// Frontend Developer</div>
+                    <div className="text-[#DD337F]">
+                        const <span className="text-[#2563EB]">developer</span>{' '}
+                        <span className="text-black dark:text-gray-500">=</span>{' '}
+                        <span className="text=[#FA9753]">{'{'}</span>
+                    </div>
+                    <div className="text-[#16A34A] text-sm pl-6">
+                        name:{' '}
+                        <span className="text-[#FA9753]">
+                            {" 'Mohaddese Karimi'"}
+                        </span>
+                    </div>
+                    <div className="text-[#16A34A] text-sm pl-6">
+                        skills: <span className="text-[#FA9753]">[</span>
+                        <span className="text-[#FA9753]">'Git'</span> ,{' '}
+                        <span className="text-[#FA9753]">'Html'</span> ,{' '}
+                        <span className="text-[#FA9753]">'Css'</span> ,{' '}
+                        <span className="text-[#FA9753]">'JavaScript'</span> ,{' '}
+                        <span className="text-[#FA9753]">'TypeScript'</span> ,{' '}
+                        <span className="text-[#FA9753]">'React'</span> ,{' '}
+                        <span className="text-[#FA9753]">'Next.Js'</span>{' '}
+                        <span className="text-[#FA9753]">]</span> ,{' '}
+                    </div>
+                    <div className="text-[#16A34A] text-sm pl-6">
+                        focuses: <span className="text-[#FA9753]">[</span>
+                        <span className="text-[#FA9753]">'Frontend'</span>{' '}
+                        <span className="text-[#FA9753]">]</span> ,{' '}
+                    </div>
+                    <div className="text-[#16A34A] text-sm pl-6">
+                        learning:{' '}
+                        <span className="text-[#FA9753]">{" 'Always'"}</span>
+                    </div>
+                    <div className="text-[#16A34A] text-sm pl-6">
+                        coffee:{' '}
+                        <span className="text-[#FA9753]">{" 'Infinity'"}</span>
+                    </div>
+                    <div className="text-[#FA9753]">{'}'}</div>
+                </div>
             </div>
+            {/* </div> */}
         </section>
     );
 }
